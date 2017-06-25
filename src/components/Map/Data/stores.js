5c95fb6bb29f96859a2a9ae70afae07a6fd8b643
Andrew Vye <a.vye26@gmail.com>
2017-06-25 15:53:34 -0700

    Add initial map view

---
+++ src/components/Map/Data/stores.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default [
+  {
+    position: {
+      lat: 46.4020734,
+      lng: -120.2619988,
+    },
+    key: `Zillah`,
+    defaultAnimation: 2,
+    showInfo: false,
+    infoContent: (
+      <p>Zillah</p>
+    ),
+  },
+  {
+    position: {
+      lat: 46.592211,
+      lng: -120.546838,
+    },
+    key: `Yakima`,
+    defaultAnimation: 2,
+    showInfo: false,
+    infoContent: (
+      <p>Yakima</p>
+    ),
+  }
+]
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,17 +1,91 @@
 import React, { Component } from 'react';
-import classnames from 'classnames';
+import { withGoogleMap, GoogleMap, InfoWindow, Marker } from 'react-google-maps';
+// import classnames from 'classnames';
 
 import './style.css';
+import markerData from './Data/stores';
+
+const SBZGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    ref={props.onMapLoad}
+    defaultZoom={6}
+    defaultCenter={{ lat: 46.592211, lng: -120.546838 }}
+  >
+    {props.markers.map((marker, index) => (
+      <Marker
+        key={index}
+        position={marker.position}
+        onClick={() => props.onMarkerClick(marker)}
+      >
+        {marker.showInfo && (
+            <InfoWindow onCloseClick={() => props.onMarkerClose(marker)}>
+              <div>{marker.infoContent}</div>
+            </InfoWindow>
+        )}
+      </Marker>
+    ))}
+  </GoogleMap>
+));
 
 export default class Map extends Component {
+  state = { markers: markerData }
+
+  handleMapLoad = this.handleMapLoad.bind(this);
+  handleMarkerClick = this.handleMarkerClick.bind(this);
+  handleMarkerClose = this.handleMarkerClose.bind(this);
+
+  handleMapLoad(map) {
+    this._mapComponent = map;
+  }
+
+  handleMarkerClick(targetMarker) {
+    if (targetMarker.showInfo) {
+      // Close info window if already shown
+      this.handleMarkerClose(targetMarker);
+    } else {
+      this.setState({
+        markers: this.state.markers.map(marker => {
+          if (marker === targetMarker) {
+            return {
+              ...marker,
+              showInfo: true,
+            };
+          }
+          return marker;
+        }),
+      });
+    }
+ }
+
+ handleMarkerClose(targetMarker) {
+   this.setState({
+     markers: this.state.markers.map(marker => {
+       if (marker === targetMarker) {
+         return {
+           ...marker,
+           showInfo: false,
+         };
+       }
+       return marker;
+     }),
+   });
+ }
 
   render() {
-    const { className, ...props } = this.props;
     return (
-      <div className={classnames('About', className)} {...props}>
-        <h1>
-          About
-        </h1>
+      <div>
+        <SBZGoogleMap
+          containerElement={
+            <div style={{ height: 400, width: 400 }} />
+          }
+          mapElement={
+            <div style={{ height: 400, width: 400 }} />
+          }
+          onMapLoad={this.handleMapLoad}
+          markers={this.state.markers}
+          onMarkerClick={this.handleMarkerClick}
+          onMarkerClose={this.handleMarkerClose}
+        />
       </div>
     );
   }
