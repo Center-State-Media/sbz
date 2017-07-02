cb10582fc91148ddd79ea3feb330ae46c7dd1cab
Andrew Vye <a.vye26@gmail.com>
2017-07-01 17:38:51 -0700

    Removes the navbar from the age check landing page

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,54 +4,59 @@ import Responsive from 'react-responsive';
 
 import './style.css';
 
-const Default = ({ children }) => <Responsive minWidth={768} className="navbarLinksContainer" children={children} />;
-const Mobile = ({ children }) => <Responsive maxWidth={768} className="navbarLinksContainer" children={children} />;
+const Default = ({ children }) => <Responsive minWidth={768} className='navbarLinksContainer' children={children} />;
+const Mobile = ({ children }) => <Responsive maxWidth={768} className='navbarLinksContainer' children={children} />;
 
-export default function NavBar() {
-  return (
-    <div className="navbarContainer">
-      <div className="navbarBrandContainer">
-        <h1>
-          Brand
-        </h1>
+export default function NavBar(props) {
+  // To remove navbar from the AgeCheck landing page
+  if (window.location.pathname !== '/') {
+    return (
+      <div className='navbarContainer'>
+        <div className='navbarBrandContainer'>
+          <h1>
+            Brand
+          </h1>
+        </div>
+        <nav className='navbarLinksOuterContainer'>
+          <Default>
+            <h1>
+              <Link to='/home'>
+                Home
+              </Link>
+            </h1>
+            <h1>
+              <Link to='/products'>
+                Products
+              </Link>
+            </h1>
+            <h1>
+              <Link to='/about'>
+                About
+              </Link>
+            </h1>
+            <h1>
+              <Link to='/map'>
+                Find a Location
+              </Link>
+            </h1>
+
+            <h1>
+              <Link to='/contact-us'>
+                Contact Us
+              </Link>
+            </h1>
+          </Default>
+          <Mobile>
+            <h1>
+              <Link to='/contact-us'>
+                Contact Us
+              </Link>
+            </h1>
+          </Mobile>
+        </nav>
       </div>
-      <nav className="navbarLinksOuterContainer">
-        <Default>
-          <h1>
-            <Link to="/home">
-              Home
-            </Link>
-          </h1>
-          <h1>
-            <Link to="/products">
-              Products
-            </Link>
-          </h1>
-          <h1>
-            <Link to="/about">
-              About
-            </Link>
-          </h1>
-          <h1>
-            <Link to="/map">
-              Find a Location
-            </Link>
-          </h1>
+    )
+  }
 
-          <h1>
-            <Link to="/contact-us">
-              Contact Us
-            </Link>
-          </h1>
-        </Default>
-        <Mobile>
-          <h1>
-            <Link to="/contact-us">
-              Contact Us
-            </Link>
-          </h1>
-        </Mobile>
-      </nav>
-    </div>
-  )
+  return null;
 }
